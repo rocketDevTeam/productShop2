0b4c1732e2cc8793af38fbcafe75f75d0bb0759d
Тарас Павленко <rockerdevteam@gmail.com>
2020-02-23 00:11:42 +0200

    Added sounds

--- main.js
+++ main.js
@@ -1,17 +1,66 @@
 const validName = "Ann";
 const validSurname = "Kuhir";
-function onSubmit() {
+const secretName = "Taras";
+const secretSurname = "Pavlenko";
+const whoAreYou = new Audio('media/who-are-you.mp3');
+const success = new Audio('media/kids-cheering.mp3');
+const thugLive = new Audio('media/thug-life.mp3');
+const onSubmit = () => {
     const form = document.querySelector('form');
     var formData = new FormData(form);
     const name = formData.get('name');
     const surname = formData.get('surname');
     if (name === validName && surname === validSurname) {
-        alert("Welcome, honey (◍•ᴗ•◍)");
-        form.style.display = 'none';
-        
+        success.play();
+        customAlert('Welcome, HONEY (◍•ᴗ•◍)', success, true)
+
+    } else if (name === secretName && surname === secretSurname) {
+        thugLive.play();
+        customAlert('He loves YOU!!!', thugLive)
+        form.reset();
     } else {
-        alert("Who are you, stranger?");
+        whoAreYou.play();
+        strangerAlert('Who are you, STRANGER?', whoAreYou)
         form.reset();
     }
-    
+
+}
+
+const customAlert = (message, sound, isLogin) => {
+    if (message) {
+        const form = document.querySelector('form');
+        form.style.display = 'none';
+        const wrapper = document.getElementById('wrapper-main');
+        const mess = document.createElement('span');
+        mess.innerText = message;
+        mess.className = 'message'
+        wrapper.append(mess)
+        const duration = sound.duration * 1000;
+        
+        setTimeout(() => {
+            mess.style.display = 'none';
+            if (!isLogin) {
+                form.style.display = 'flex';
+            }
+        }, duration);
+    }
+}
+
+const strangerAlert = (message, sound) => {
+    if (message) {
+        const form = document.querySelector('form');
+        form.style.display = 'none';
+        const wrapper = document.getElementById('wrapper-main');
+        const mess = document.createElement('span');
+        mess.innerText = message;
+        mess.className = 'message';
+        wrapper.append(mess)
+        const duration = sound.duration * 1000;
+        setTimeout(() => {
+            mess.style.display = 'none';
+            if (!isLogin) {
+                form.style.display = 'flex';
+            }
+        }, duration);
+    }
 }
