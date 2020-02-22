bbb31a92e18d6b893f93ef974ff40809fc2381b6
daria.kochanova@anuitex.com <daria.kochanova@anuitex.com>
2020-02-22 20:19:11 +0200

    Added login function

---
+++ main.js
@@ -0,0 +1,14 @@
+const validName = "Ann";
+const validSurname = "Kuhir";
+function onSubmit() {
+    const form = document.querySelector('form');
+    var formData = new FormData(form);
+    const name = formData.get('name');
+    const surname = formData.get('surname');
+    if (name === validName && surname === validSurname) {
+        alert("Welcome, honey (◍•ᴗ•◍)");
+        form.style.display = 'none'
+    } else {
+        alert("Who are you, stranger?");
+    }
+}
