499d6d550e7aa05c291dd11750ae09a4724aa409
Тарас Павленко <rockerdevteam@gmail.com>
2020-02-23 11:29:21 +0200

    candle

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ const secretSurname = "Pavlenko";
 const whoAreYou = new Audio('media/who-are-you.mp3');
 const success = new Audio('media/kids-cheering.mp3');
 const thugLive = new Audio('media/thug-life.mp3');
+const happyBirthday = new Audio('media/happy-birthday.mp3');
+let wrapper = null
 const onSubmit = () => {
+    wrapper = document.getElementById('wrapper-main');
     const form = document.querySelector('form');
     var formData = new FormData(form);
     const name = formData.get('name');
     const surname = formData.get('surname');
     if (name === validName && surname === validSurname) {
         success.play();
-        customAlert('Welcome, HONEY (◍•ᴗ•◍)', success, true)
+        customAlert('Welcome, HONEY (◍•ᴗ•◍)', success, true);
 
     } else if (name === secretName && surname === secretSurname) {
         thugLive.play();
@@ -25,8 +28,7 @@ const onSubmit = () => {
     }
 
 }
-
-const customAlert = (message, sound, isLogin) => {
+const customAlert = async (message, sound, isLogin) => {
     if (message) {
         const form = document.querySelector('form');
         form.style.display = 'none';
@@ -41,16 +43,23 @@ const customAlert = (message, sound, isLogin) => {
             mess.style.display = 'none';
             if (!isLogin) {
                 form.style.display = 'flex';
+            } else{
+                showCake();
             }
         }, duration);
     }
+}
+
+const showCake = () => {
+    debugger;
+    wrapper.style.backgroundColor = 'black';
+    happyBirthday.play();
 }
 
 const strangerAlert = (message, sound) => {
     if (message) {
         const form = document.querySelector('form');
         form.style.display = 'none';
-        const wrapper = document.getElementById('wrapper-main');
         const img = document.createElement('img');
         img.src = 'media/suspect.png'
         img.className = 'stranger-img';
